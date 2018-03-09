cf42129bf956cc274684df386e7eb16e99d7b7dd
orion-z <x+orion-z@users.noreply.github.com>
2018-03-09 20:51:54 +0100

    added new project

--- projects/js/main.js
+++ projects/js/main.js
@@ -2,3 +2,23 @@ $(".details").on("click", function() {
   $(this).siblings("p").slideToggle(600);
   $(this).toggleClass("active");
 })
+
+let modal = $("#myModal");
+
+$("#modalBtn").off().on("click", function() {
+  modal.css("display", "block");
+})
+
+$(modal).on("click", function() {
+  modal.css("display", "none");
+  window.location.href = "patatap/index.html";
+})
+
+// $(window).on("click", function() {
+//   if (event.target == modal) {
+//     modal.css("display", "none");
+//     window.location.href = "patatap/index.html";
+//   }
+// })
+
+//patatap/index.html
